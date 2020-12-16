754ffec19f3e2f100af6f1e73d8fadcfaab5fd66
akinator001 <aakashgandhi1279@gmail.com>
2020-12-17 01:07:30 +0530

    [aakash]Added Lombok Fuctionality , Removed Getter and Setter

--- src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,44 +1,24 @@
 package com.employeepayroll.dto;
 
+import java.time.LocalDate;
+
 import com.employeepayroll.model.EmployeePayrollData;
 
+import lombok.Data;
+
+@Data
 public class EmployeePayrollDTO {
 	private Long id;
 	private String name;
-	private String basic_pay;
-
+	private String basicPay;
+	private LocalDate startDate;
+	private Character gender;	
+	
 	public EmployeePayrollDTO(EmployeePayrollData employeePayroll) {
 		this.setId(employeePayroll.getId());
 		this.setName(employeePayroll.getName());
-		this.setSalary(employeePayroll.getBasic_pay());
-	}
-	public EmployeePayrollDTO() {}
-
-	public String toString() {
-		return "Name ="+this.name+", salary="+this.basic_pay; 
+		this.setBasicPay(employeePayroll.getBasicPay());
 	}
 	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getBasic_pay() {
-		return basic_pay;
-	}
-
-	public void setSalary(String salary) {
-		this.basic_pay = salary;
-	}
+	public EmployeePayrollDTO() {}
 }
