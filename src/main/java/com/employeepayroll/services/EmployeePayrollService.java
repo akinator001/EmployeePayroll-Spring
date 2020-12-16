3e4f0cacf0a2e45c9aac8a68090864ba5b5c9f75
akinator001 <aakashgandhi1279@gmail.com>
2020-12-17 00:20:55 +0530

    [aakash]Added Lombok Fuctionality , Removed Getter and Setter

--- src/main/java/com/employeepayroll/services/EmployeePayrollService.java
+++ src/main/java/com/employeepayroll/services/EmployeePayrollService.java
@@ -2,45 +2,58 @@ package com.employeepayroll.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.employeepayroll.dto.EmployeePayrollDTO;
+import com.employeepayroll.exceptions.DetailsNotProvidedExceptions;
+import com.employeepayroll.exceptions.UserNotFound;
 import com.employeepayroll.model.EmployeePayrollData;
+import com.employeepayroll.repository.EmployeePayrollRepository;
 
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService{
+	@Autowired
+    private EmployeePayrollRepository employeePayrollRepository;
+	
+	public List<EmployeePayrollDTO> getAllUser(){
+		return employeePayrollRepository.findAll().stream()
+				.map(employeePayroll -> new EmployeePayrollDTO(employeePayroll))
+				.collect(Collectors.toList());
+    }
 
-	private List<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
-
+	
 	@Override
-	public List<EmployeePayrollData> getEmployeeData() {
-		return employeePayrollList;
+	public EmployeePayrollDTO createUser(EmployeePayrollDTO employeePayrollDTO) {
+		if(Objects.nonNull(employeePayrollDTO.getName()) && Objects.nonNull(employeePayrollDTO.getBasicPay())){
+			EmployeePayrollData employeePayroll = new EmployeePayrollData(employeePayrollDTO.getName(), employeePayrollDTO.getBasicPay(),employeePayrollDTO.getGender(),employeePayrollDTO.getStartDate());
+			return new EmployeePayrollDTO(employeePayrollRepository.save(employeePayroll));
+		}
+		throw new DetailsNotProvidedExceptions("Invalid Data");
 	}
 
 	@Override
-	public EmployeePayrollData getEmployeePayrollById(int empId) {
-		return employeePayrollList.get(empId-1);
+	public EmployeePayrollDTO updateUser(EmployeePayrollDTO employeePayrollDTO) {
+		return employeePayrollRepository.findById(employeePayrollDTO.getId()).map(employeePayroll -> {
+			if(Objects.nonNull(employeePayrollDTO.getName())) {
+				employeePayroll.setName(employeePayrollDTO.getName());
+			}
+			if(Objects.nonNull(employeePayrollDTO.getBasicPay())) {
+				employeePayroll.setBasicPay(employeePayrollDTO.getBasicPay());
+			}
+			return new EmployeePayrollDTO(employeePayrollRepository.save(employeePayroll));
+		}).orElseThrow(() -> new UserNotFound("UserNotFound"));
 	}
 
 	@Override
-	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(employeePayrollList.size()+1, empPayrollDTO);
-		return empData;
-	}
-
-	@Override
-	public EmployeePayrollData updateEmployeePayrollData(int empId,EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData empData = employeePayrollList.get(empId-1);
-		empData.setName(empPayrollDTO.name);
-		empData.setSalary(empPayrollDTO.salary);
-		return empData;
-	}
-
-	@Override
-	public void deleteEmployeePayrollData(int empId) {
-		employeePayrollList.remove(empId-1);
+	public EmployeePayrollDTO deleteUser(Long id) {
+		return employeePayrollRepository.findById(id).map(employeePayroll -> {
+			employeePayrollRepository.deleteById(employeePayroll.getId());
+			return new EmployeePayrollDTO(employeePayroll);
+		}).orElseThrow(() -> new UserNotFound("UserNOtFound"));
 	}
 
 }
